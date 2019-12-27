8b52e5854ccac4c73b5fe8b8cf3e758f8710d47c
Bryan Rhodunda <rhodunda@udel.edu>
2019-12-27 23:46:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,7 +26,7 @@ def gross_for_director(director_data)
   index = 0 
   while index < director_data[:movies].lenght do 
     total += director_data[:movies][index][:worldwide_gross]
-    index += 
+    index += 1
   end 
   
   
